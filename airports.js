975bbbc3216449380f6a5d8a9b78e4b149c50fed
kejvi-avenuecode <121968175+kejvi-avenuecode@users.noreply.github.com>
2023-01-04 12:29:08 -0500

    Create airports.js

---
+++ airports.js
@@ -0,0 +1,60 @@
+{
+  "airports": [
+    {
+        "code":"LAX",
+        "name": "LA Airport",
+		"capacity" : 25486,
+		"planes": 4785,
+		"employees" : 78456,
+		"image": "https://a.cdn-hotels.com/gdcs/production191/d1280/7a0b84d3-96b1-41db-ac20-a2da396fec86.jpg"
+    },
+    {
+        "code": "DEN",
+        "name": "Denver Airport",
+		"capacity" : 4685,
+		"planes": 7896,
+		"employees" : 123,
+		"image": "https://cdn.5280.com/2021/11/DIA-main-terminal-.jpg"
+    },
+    {
+        "code": "YYZ",
+        "name" : "Pearson Airport",
+		"capacity" : 3954827,
+		"planes": 23124,
+		"employees" : 534,
+		"image": "https://blog.blacklane.com/wp-content/uploads/2019/07/Untitled-design-2019-07-16T170844.857.png"
+    },
+    {
+        "code": "YVR",
+        "name": "Vancouver Airport",
+		"capacity" : 98475,
+		"planes": 2343,
+		"employees" : 138,
+		"image": "https://s28477.pcdn.co/wp-content/uploads/2018/01/YVR_1-984x554.jpg"
+    },
+    {
+        "code": "YUL",
+        "name": "Montreal Airport",
+		"capacity" : 12434,
+		"planes": 453,
+		"employees" : 223,
+		"image": "https://images.dailyhive.com/20210916133612/trudeau-airport.jpg"
+    },
+    {
+        "code": "YHZ",
+        "name": "Halifax Airport",
+		"capacity" : 12434,
+		"planes": 453,
+		"employees" : 223,
+		"image": "https://discoverhalifaxns.com/wp-content/uploads/2021/04/Halifax-Stanfield-Airport.jpeg"
+    },
+    {
+        "code": "YEG",
+        "name": "Edmonton Airport",
+		"capacity" : 12434,
+		"planes": 453,
+		"employees" : 223,
+		"image": "https://flyeia.com/corporate/wp-content/uploads/EIA-50-1024x866.jpg"
+    }
+  ]
+}
